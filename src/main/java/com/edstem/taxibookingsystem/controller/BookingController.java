79d0e4020156f5be3378cd8a5f29f9b1263ffeda
devanand-jrx <devanand.ravindran@edstem.com>
2024-02-01 18:01:27 +0530

    view booking and cancel booking updated

--- src/main/java/com/edstem/taxibookingsystem/controller/BookingController.java
+++ src/main/java/com/edstem/taxibookingsystem/controller/BookingController.java
@@ -5,6 +5,9 @@ import com.edstem.taxibookingsystem.contract.response.BookingResponse;
 import com.edstem.taxibookingsystem.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,16 @@ public class BookingController {
     @PostMapping
     public @ResponseBody BookingResponse addBooking(@RequestBody BookingRequest bookingRequest){
         return bookingService.addBooking(bookingRequest);
+    }
+
+    @GetMapping("/{bookingId}")
+    public @ResponseBody BookingResponse viewBooking(@PathVariable Long bookingId){
+        return bookingService.viewBooking(bookingId);
+    }
+
+    @DeleteMapping("/{bookingId}")
+    public void cancelBooking(@PathVariable Long bookingId){
+        bookingService.cancelBooking(bookingId);
     }
 
 }
--- src/main/java/com/edstem/taxibookingsystem/service/BookingService.java
+++ src/main/java/com/edstem/taxibookingsystem/service/BookingService.java
@@ -2,6 +2,7 @@ package com.edstem.taxibookingsystem.service;
 
 import com.edstem.taxibookingsystem.contract.request.BookingRequest;
 import com.edstem.taxibookingsystem.contract.response.BookingResponse;
+import com.edstem.taxibookingsystem.exception.BookingNotFoundException;
 import com.edstem.taxibookingsystem.model.Booking;
 import com.edstem.taxibookingsystem.repository.BookingRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,21 @@ public class BookingService {
                 .build();
         booking = bookingRepository.save(booking);
         return modelMapper.map(booking, BookingResponse.class);
+    }
+
+
+    public BookingResponse viewBooking(Long bookingId){
+        Booking booking = bookingRepository
+                .findById(bookingId)
+                .orElseThrow(() -> new BookingNotFoundException("Booking Not Found"));
+        return modelMapper.map(booking, BookingResponse.class);
+    }
+
+    public void cancelBooking(Long bookingId){
+        if(!bookingRepository.existsById(bookingId)){
+            throw new BookingNotFoundException("Booking Not Found");
+        }
+        bookingRepository.deleteById(bookingId);
     }
 
 
