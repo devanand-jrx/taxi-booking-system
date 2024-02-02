e2c05ff23dd8f7da66cfb4ea58274c8fdf2188ae
devanand-jrx <devanand.ravindran@edstem.com>
2024-02-02 17:40:15 +0530

    updated

---
+++ README.md
@@ -0,0 +1,2 @@
+# taxi-booking-system
+The Taxi Booking System is a Java Spring Boot project, which allows users to book taxis, calculate fares, and handle billing
--- src/test/java/com/edstem/taxibookingsystem/controller/UserControllerTest.java
+++ src/test/java/com/edstem/taxibookingsystem/controller/UserControllerTest.java
@@ -14,8 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
