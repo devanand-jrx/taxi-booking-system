169e4965a4d41e4bfd517cac98a8fcd6ab882b3a
devanand-jrx <devanand.ravindran@edstem.com>
2024-02-06 14:06:34 +0530

    updated

--- src/main/java/com/edstem/taxibookingsystem/contract/response/UserResponse.java
+++ src/main/java/com/edstem/taxibookingsystem/contract/response/UserResponse.java
@@ -3,10 +3,12 @@ package com.edstem.taxibookingsystem.contract.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
     private String userId;
     private String name;
